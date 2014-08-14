f748ef1357f4a2c4f1cbfcaac896c47c046a557b
Prelang Builder <builder@prelang.com>
2014-08-14 03:53:15 +0000

    Installed relation 'has_one' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_one :coach
+  has_one :athlete
 end
